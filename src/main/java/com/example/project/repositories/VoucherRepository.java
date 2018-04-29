11baba4c44045819146c016898a9fd3a877b3d2f
Milos Tepic <milos837@gmail.com>
2018-04-29 21:40:59 +0200

    obrisao komentar

--- src/main/java/com/example/project/repositories/VoucherRepository.java
+++ src/main/java/com/example/project/repositories/VoucherRepository.java
@@ -10,11 +10,9 @@ import com.example.project.entities.VoucherEntity;
 
 public interface VoucherRepository extends CrudRepository<VoucherEntity, Integer> {
 	
-	//treba testirati
 	@Query("SELECT v FROM VoucherEntity v JOIN UserEntity u ON v.user=u.id WHERE u.id = ?1")
 	List<VoucherEntity> findByBuyerCustomQuery(Integer buyerId);
 	
-	//treba testirati
 	@Query("SELECT v FROM VoucherEntity v JOIN OfferEntity o ON v.offer=o.id WHERE o.id = ?1")
 	List<VoucherEntity> findByOfferCustomQuery(Integer offerId);
 	
